7cfe58b1857e29b5ff30d771b13d21516db2a008
Renato Herebia <rherebia@gmail.com>
2017-04-17 22:29:10 -0400

    Assistindo aula 3

--- src/main/java/br/com/caelum/jms/TesteConsumidor.java
+++ src/main/java/br/com/caelum/jms/TesteConsumidor.java
@@ -5,9 +5,12 @@ import java.util.Scanner;
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
 import javax.jms.Destination;
+import javax.jms.JMSException;
 import javax.jms.Message;
 import javax.jms.MessageConsumer;
+import javax.jms.MessageListener;
 import javax.jms.Session;
+import javax.jms.TextMessage;
 import javax.naming.InitialContext;
 
 public class TesteConsumidor {
@@ -23,11 +26,21 @@ public class TesteConsumidor {
 		Destination fila = (Destination) context.lookup("financeiro");
 		MessageConsumer consumer = session.createConsumer(fila);
 		
-		Message message = consumer.receive();
+		consumer.setMessageListener(new MessageListener() {
+			
+			@Override
+			public void onMessage(Message message) {
+				TextMessage textMessage = (TextMessage) message;
+				
+				try {
+					System.out.println("Recebendo msg " + textMessage.getText());
+				} catch (JMSException e) {
+					e.printStackTrace();
+				}
+			}
+		});
 		
-		System.out.println("Recebendo msg " + message);
-		
-		new Scanner(System.in).next();
+		new Scanner(System.in).nextLine();
 		
 		session.close();
 		connection.close();
